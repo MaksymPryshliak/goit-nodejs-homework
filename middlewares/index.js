816fe61d3a3828faae440d5ba269e15e133766b5
MaksymPryshliak <115544382+MaksymPryshliak@users.noreply.github.com>
2023-05-05 01:27:38 +0300

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,7 @@
 const validateBody = require("./validateBody");
 const validateId = require("./validateId");
-const authentification = require("./authentification");
 
 module.exports = {
   validateBody,
   validateId,
-  authentification,
 };
