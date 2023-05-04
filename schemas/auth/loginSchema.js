17e620846f8974a73c15278f733c665e354f05c1
MaksymPryshliak <115544382+MaksymPryshliak@users.noreply.github.com>
2023-05-05 01:29:21 +0300

    Delete schemas/auth directory

--- schemas/auth/loginSchema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi");
-const emailRegex = /^([a-zA-Z0-9-.]+)@([a-zA-Z0-9-.]+).([a-zA-z]+)$/;
-
-const loginSchema = Joi.object({
-  email: Joi.string().pattern(emailRegex).required(),
-  password: Joi.string().min(6).required(),
-});
-
-module.exports = loginSchema;
--- schemas/auth/subscribeSchema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi");
-const subEnum = ["starter", "pro", "business"];
-
-const subscribeSchema = Joi.object({
-  subscription: Joi.string()
-    .valid(...subEnum)
-    .required(),
-});
-module.exports = subscribeSchema;
