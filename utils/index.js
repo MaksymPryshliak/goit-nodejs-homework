846e048c309c54973213202e13e4621506bd088f
MaksymPryshliak <115544382+MaksymPryshliak@users.noreply.github.com>
2023-05-05 01:30:26 +0300

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,7 @@
 const tryCatchWrapper = require("./tryCatchWrapper");
 const requestError = require("./requestError");
-const handleMongooseError = require("./handleMongooseError");
 
 module.exports = {
   tryCatchWrapper,
   requestError,
-  handleMongooseError,
 };
